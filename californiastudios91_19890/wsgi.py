d507770d52667c503a2455433d4c7244de0ecbf4
Crowdbotics <team@crowdbotics.com>
2020-08-30 23:00:49 +0000

    Update models

--- californiastudios91_19890/wsgi.py
+++ californiastudios91_19890/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'californiastudios91_19890.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "californiastudios91_19890.settings")
 
 application = get_wsgi_application()
